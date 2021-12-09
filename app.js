addc085b6629813a790b24f33a39642095fc7f59
ikkiwar <ikkiofbodom@gmail.com>
2021-12-09 16:01:49 -0600

    hhtps added

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 // Módulos requeridos
 const express = require("express");
+const fs = require("fs");
 const http = require("http");
+const https = require("https");
 const helmet = require("helmet");
 var compression = require("compression");
 require("dotenv").config();
 const { v4: uuidv4 } = require("uuid");
 const cors = require("cors");
+
+const httpsServerOptions = {
+  key: fs.readFileSync(process.env.KEY_PATH),
+  cert: fs.readFileSync(process.env.CERT_PATH),
+};
 
 const app = express();
 app.use(helmet()); // Ayuda a proteger aplicaciones Express
@@ -14,6 +21,17 @@ app.use(cors());
 // Servidor HTTP
 const serverHttp = http.createServer(app);
 serverHttp.listen(process.env.HTTP_PORT, process.env.IP);
+
+const serverHttps = https.createServer(httpsServerOptions, app);
+serverHttps.listen(process.env.HTTPS_PORT, process.env.IP);
+
+app.use((req, res, next) => {
+  if (req.secure) {
+    next();
+  } else {
+    res.redirect("https://" + req.headers.host + req.url);
+  }
+});
 
 app.use(express.static("./public"));
 // Prueba
